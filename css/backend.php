ea24c000cc641bbdf0659a29c62401b066229d3e
luisfwa <luisfernandowadtassis@gmail.com>
2016-08-28 11:36:03 -0300

    Add files via upload
    Initial commit

---
+++ css/backend.php
@@ -0,0 +1,5 @@
+<div id="aproxime">
+<div id="inside">
+<center><img src="img/logo_paygo.png"><br><hr style="margin-top: -10px">
+<img src="img/nfc_waiting.png"></center><br>
+<center>APROXIME O PRODUTO</center><br>
---
+++ js/dyn.js
@@ -0,0 +1,11 @@
+var subtotal = 0;
+
+window.onload = function()	{
+	document.setInterval(callback, 200);
+}
+
+function callback()	{
+	$.post("http://www.conwebdesign.com.br/PayGo/get_carrinho", function(result)	{
+		
+	})
+}
